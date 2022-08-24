c7ceea1929d06a13d7f9d6377a791ec2e27c933b
Cameron Jenkins <cameronj1205@gmail.com>
2022-08-24 20:27:41 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/example-org-a8a18b/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "camresource"
+  resource_group_name = "${var.prefix}-workshop"
 }
